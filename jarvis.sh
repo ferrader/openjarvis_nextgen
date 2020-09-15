17ba9779827f74f02a420ca2500eccd190911f83
ferrader <ferrari.jerome@gmail.com>
2020-09-15 14:48:27 +0200

    Readd plugins store in hard
    Update function of plugins store don't work. I reimplented directly the json file

--- jarvis.sh
+++ jarvis.sh
@@ -93,7 +93,8 @@ lockfile="$jv_cache_folder/jarvis.lock"
 audiofile="$jv_cache_folder/jarvis-record.wav"
 forder="$jv_cache_folder/jarvis-order"
 jv_say_queue="$jv_cache_folder/jarvis-say"
-jv_store_file="$jv_cache_folder/jarvis-store.json"
+#jv_store_file="$jv_cache_folder/jarvis-store.json"
+jv_store_file="/plugins/jarvis-store.json"
 rm -f $audiofile # sometimes, when error, previous recording is played
 if [ ! -d "plugins_installed" ]; then
     if [ -d "plugins" ]; then # retrocompatibility
