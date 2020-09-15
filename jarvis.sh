ad10ce0f9d09f8e3536c0343506fea375e6eddc3
ferrader <ferrari.jerome@gmail.com>
2020-09-15 17:49:26 +0200

    correction store

--- jarvis.sh
+++ jarvis.sh
@@ -93,8 +93,10 @@ lockfile="$jv_cache_folder/jarvis.lock"
 audiofile="$jv_cache_folder/jarvis-record.wav"
 forder="$jv_cache_folder/jarvis-order"
 jv_say_queue="$jv_cache_folder/jarvis-say"
-#jv_store_file="$jv_cache_folder/jarvis-store.json"
-jv_store_file="/plugins/jarvis-store.json"
+
+cp "$jv_dir/plugins/jarvis-store.json" "$jv_cache_folder/jarvis-store.json"
+jv_store_file="$jv_cache_folder/jarvis-store.json"
+
 rm -f $audiofile # sometimes, when error, previous recording is played
 if [ ! -d "plugins_installed" ]; then
     if [ -d "plugins" ]; then # retrocompatibility
